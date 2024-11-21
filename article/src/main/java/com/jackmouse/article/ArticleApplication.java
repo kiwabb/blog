4263b0c10c443c0b36bf8d592de047bed56e2601
周江尧 <1310179240@qq.com>
2024-11-21 16:50:03 +0800

    feat[系统搭建]: 集成gateway和nacos

--- article/src/main/java/com/jackmouse/article/ArticleApplication.java
+++ article/src/main/java/com/jackmouse/article/ArticleApplication.java
@@ -3,8 +3,10 @@ package com.jackmouse.article;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @MapperScan("com.jackmouse.article.mapper")
 public class ArticleApplication {
 
